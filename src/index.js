371d2955f6e4f7c76b437ed3b6fd364d803316d8
Anastasiia Zahuliaieva <nastyazagyl2@gmail.com>
2020-10-15 21:56:38 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 
 // You should implement your task here.
+// @ts-check
+/**
+ * 
+ * @param {number[][]} matrix 
+ */
+module.exports = towelSort;
 
-module.exports = function towelSort (matrix) {
-  return [];
+function towelSort (matrix) {
+  if(!matrix) {
+    return [];
+  }
+  let result = [];
+  for (let i=0; i < matrix.length; i++) {
+    if(i === 0 || i % 2 == 0) {
+       result = [...result, ...matrix[i]];
+    } else {
+     let rever = matrix[i].reverse();
+      result = [...result,...rever];
+    }
+  }
+  return result;
 }
+
+console.log(towelSort([
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+]));
+
+
